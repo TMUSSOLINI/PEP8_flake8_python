d451b5747a3980c6e56593f6dc4da6101e62ad02
Thiago Mussolini <mussolinithiago1994@gmail.com>
2022-08-04 10:39:38 -0300

    criando fila base

---
+++ fila_base.py
@@ -0,0 +1,11 @@
+class FilaBase:
+    codigo = 0
+    fila = []
+    clientes_atendidos = []
+    senha_atual:str = ''
+
+    def reseta_fila(self) -> None:
+        if self.codigo >= 200:
+            self.codigo = 0
+        else:
+            self.codigo += 1
--- fila_normal.py
+++ fila_normal.py
@@ -1,24 +1,17 @@
-class Filanormal:
-    codigo = 0
-    fila = []
-    clientesAtendidos = []
-    senhaAtual:str = ''
+from fila_base import FilaBase
+
+class Filanormal(FilaBase):
 
     def geraSenhaAtual(self) -> None:
         self.senhaAtual = f'NM{self.codigo}'
 
-    def resetaFila(self) -> None:
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
 
     def atualizaFila(self) -> None:
-        self.resetaFila()
+        self.reseta_fila()
         self.geraSenhaAtual()
         self.fila.append(self.senhaAtual)
 
     def chamaCliente(self, caixa:int) -> str:
         cliente_atual:str = self.fila.pop(0)
-        self.clientesAtendidos.append(cliente_atual)
+        self.clientes_atendidos.append(cliente_atual)
         return (f'cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
